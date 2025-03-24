123e94509a918fc70bf83ebba1d7a3ce9592993f
Tollan Berhanu <tollanberhanu@gmail.com>
2025-03-24 16:52:51 -0600

    fix: create and rewrite logs

--- logs.py
+++ logs.py
@@ -1,13 +1,18 @@
 import socket
 
 HOST = '' # Listen on all interfaces 
-PORT = 1234 
+PORT = 2345 
 LOGFILE = 'neuron_logs.txt'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
     s.bind((HOST, PORT)) 
     s.listen() 
     print(f"Logging server listening on port {PORT}, logging to {LOGFILE}") 
+    
+    # Create and/or clear the logfile first
+    with open(LOGFILE, 'w') as f:
+        f.write('')
+
     while True: 
         conn, addr = s.accept() 
         with conn: 
